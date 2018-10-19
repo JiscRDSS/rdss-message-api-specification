f8955c76137f71990f88a8e87319fc826cad8b04
Finlay McCourt <finlay.mccourt@digirati.com>
2018-10-19 15:00:53 +0100

    Thesis must have nested properties with correct enums.

--- message-api-schema-validator/tests/body/test_thesis_dissertation.py
+++ message-api-schema-validator/tests/body/test_thesis_dissertation.py
@@ -5,6 +5,21 @@ def test_thesis_dissertation_is_valid(valid_file_against_schema):
         '../messages/body/metadata/create/thesis_dissertation_create_request.json'
     )
 
+def test_thesis_dissertation_without_awarded_date_is_invalid(load_json, invalid_against_schema):
+    message_body = load_json('../messages/body/metadata/create/thesis_dissertation_create_request.json')
+    message_body["objectDate"] = [i for i in message_body["objectDate"] if i["dateType"] != "awarded"]
+    invalid_against_schema(
+        'https://www.jisc.ac.uk/rdss/schema/thesis_dissertation.json/#/definitions/ThesisDissertation',
+        message_body
+    )
+
+def test_thesis_dissertation_without_awarding_institution_is_invalid(load_json, invalid_against_schema):
+    message_body = load_json('../messages/body/metadata/create/thesis_dissertation_create_request.json')
+    message_body["objectOrganisationRole"] = [i for i in message_body["objectOrganisationRole"] if i["role"] != "awardingInstitution"]
+    invalid_against_schema(
+        'https://www.jisc.ac.uk/rdss/schema/thesis_dissertation.json/#/definitions/ThesisDissertation',
+        message_body
+    )
 
 def test_article_is_not_a_valid_thesis_dissertation(invalid_file_against_schema):
     invalid_file_against_schema(
