8c290d8d8af7123175d1c0719d75d4f2921fdd02
Finlay McCourt <finlay.mccourt@digirati.com>
2018-11-28 15:46:16 +0000

    Having run pre-commit.

--- message-api-schema-validator/tests/test_header.py
+++ message-api-schema-validator/tests/test_header.py
@@ -5,11 +5,13 @@ def test_metadata_create_header_is_valid(valid_file_against_schema):
         '../messages/header/metadata_create_header.json'
     )
 
+
 def test_metadata_update_header_is_valid(valid_file_against_schema):
     valid_file_against_schema(
         'https://www.jisc.ac.uk/rdss/schema/message/header.json/#/definitions/Header',
         '../messages/header/metadata_update_header.json'
     )
+
 
 def test_metadata_read_header_is_valid(valid_file_against_schema):
     valid_file_against_schema(
@@ -17,17 +19,20 @@ def test_metadata_read_header_is_valid(valid_file_against_schema):
         '../messages/header/metadata_read_header.json'
     )
 
+
 def test_metadata_delete_header_is_valid(valid_file_against_schema):
     valid_file_against_schema(
         'https://www.jisc.ac.uk/rdss/schema/message/header.json/#/definitions/Header',
         '../messages/header/metadata_delete_header.json'
     )
 
+
 def test_metadata_create_error_header_is_valid(valid_file_against_schema):
     valid_file_against_schema(
         'https://www.jisc.ac.uk/rdss/schema/message/header.json/#/definitions/Header',
         '../messages/header/metadata_create_error_header.json'
     )
+
 
 def test_preservation_event_header_is_valid(valid_file_against_schema):
     valid_file_against_schema(
