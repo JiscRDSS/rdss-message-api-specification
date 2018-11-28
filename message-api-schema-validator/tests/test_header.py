bc6490ee82dfd582134097978f9262e8dbfbe950
Finlay McCourt <finlay.mccourt@digirati.com>
2018-11-28 15:27:06 +0000

    Additional header documentation and tests.

--- message-api-schema-validator/tests/test_header.py
+++ message-api-schema-validator/tests/test_header.py
@@ -1,6 +1,18 @@
 
-def test_header_is_valid(valid_file_against_schema):
+def test_metadata_create_header_is_valid(valid_file_against_schema):
     valid_file_against_schema(
         'https://www.jisc.ac.uk/rdss/schema/message/header.json/#/definitions/Header',
-        '../messages/header/header.json'
+        '../messages/header/metadata_create_header.json'
+    )
+
+def test_metadata_create_error_header_is_valid(valid_file_against_schema):
+    valid_file_against_schema(
+        'https://www.jisc.ac.uk/rdss/schema/message/header.json/#/definitions/Header',
+        '../messages/header/metadata_create_error_header.json'
+    )
+
+def test_preservation_event_header_is_valid(valid_file_against_schema):
+    valid_file_against_schema(
+        'https://www.jisc.ac.uk/rdss/schema/message/header.json/#/definitions/Header',
+        '../messages/header/preservation_event_header.json'
     )
